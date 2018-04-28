1276f1cdce055ae6e97a2803a1280d5987705be7
marwamero <37839043+marwamero@users.noreply.github.com>
2018-04-28 17:26:54 +0200

    Add files via upload

---
+++ Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    private bool left = true;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(moveLeft());
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (left)
+            transform.position = new Vector2(transform.position.x + 0.01f, transform.position.y);
+        else
+            transform.position = new Vector2(transform.position.x - 0.01f, transform.position.y);
+
+
+
+    }
+
+    IEnumerator moveLeft() {
+        left = true;
+        yield return new WaitForSeconds(2);
+        StartCoroutine(moveRight());
+
+    }
+
+    IEnumerator moveRight()
+    {
+        left = false;
+        yield return new WaitForSeconds(2);
+        StartCoroutine(moveLeft());
+
+    }
+}
---
+++ RoadGenerator (1).cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadGenerator : MonoBehaviour
+{
+    public Transform playerPosition;
+    public GameObject[] Roads;
+    public GameObject[] hurdles;
+    float LengthOfRoad = 9.81f;
+    public float RoadGeneratorTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        SpawnRoad();
+        spawnhurdles();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator generator()
+    {
+        yield return new WaitForSeconds(RoadGeneratorTime);
+        SpawnRoad();
+    }
+
+    void SpawnRoad()
+    {
+        int randomRoad = UnityEngine.Random.Range(0, Roads.Length - 1);
+        float playerxPos = playerPosition.position.x + LengthOfRoad;
+        
+        Instantiate(Roads[randomRoad], new Vector2(playerxPos, -0.65f), Roads[randomRoad].transform.rotation);
+        
+        StartCoroutine(generator());
+
+    }
+    IEnumerator spawnHurdles()
+    {
+        yield return new WaitForSeconds(3);
+        spawnhurdles();
+
+    }
+
+    void spawnhurdles()
+    {
+        int randomHurdles = UnityEngine.Random.Range(0, hurdles.Length-1);
+        float playerxPos = playerPosition.position.x + LengthOfRoad-4;
+        Instantiate(hurdles[randomHurdles],new Vector2(playerxPos, hurdles[randomHurdles].transform.position.y), hurdles[randomHurdles].transform.rotation);
+        StartCoroutine(spawnHurdles());
+    }
+
+
+
+
+
+}
