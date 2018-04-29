aeec0b498dc0517d7b91a49d983e761e898de15e
marwamero <37839043+marwamero@users.noreply.github.com>
2018-04-29 03:48:56 +0200

    Add files via upload

---
+++ Destroy (1).cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+    float distance;
+    public Transform Player;
+
+    /*void Start()
+    {
+        distance = Player.transform.position.x - transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector2(Player.transform.position.x - distance , 0);
+
+    }*/
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        Destroy(col.gameObject);
+    }
+}
